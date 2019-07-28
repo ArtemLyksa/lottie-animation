66f28ae53ae645f2e7272a997da2fcf531eb1cd1
Artem <artemlyksa@gmail.com>
2019-07-28 21:09:54 +0300

    Implemented basic animation using Lottie and bodymovin son.

--- lottie-animation/ViewController.swift
+++ lottie-animation/ViewController.swift
@@ -6,15 +6,26 @@
 //  Copyright © 2019 lyksa. All rights reserved.
 //
 
-import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animationView: AnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { [weak self] in
+            self?.setupAnimation()
+        }
         // Do any additional setup after loading the view.
     }
 
+    private func setupAnimation() {
+        animationView.animation = Animation.named("lottie-animation")
+        animationView.play { (finished) in
+            /// Animation finished
+        }
+    }
 
 }
 
