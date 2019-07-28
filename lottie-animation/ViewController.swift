9a80d53e18a37df63ab0d40546ceafd61a67fee2
Artem <artemlyksa@gmail.com>
2019-07-28 20:38:52 +0300

    initial commit

---
+++ lottie-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lottie-animation
+//
+//  Created by Artem Lyksa on 7/28/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
